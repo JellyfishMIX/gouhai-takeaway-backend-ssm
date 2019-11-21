a046f1be13e1e89c2e3f5e62b0ae009d9758f87a
JellyfishMIX <2273516339@qq.com>
2019-11-21 17:38:25 +0800

    :bug: Add annotations to fix the bug of returning the date returned to the frontend to a number

--- src/main/java/com/jellyfishmix/gouhaitakeaway/entity/Order.java
+++ src/main/java/com/jellyfishmix/gouhaitakeaway/entity/Order.java
@@ -1,5 +1,7 @@
 package com.jellyfishmix.gouhaitakeaway.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 import java.util.List;
 
@@ -10,6 +12,7 @@ public class Order {
     private String customerPhone;
     private String customerAddr;
     private Integer totalPrice;
+    @JsonFormat(pattern="yyyy-MM-dd   HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
     private List<OrderCommodity> orderCommodityList;
 
